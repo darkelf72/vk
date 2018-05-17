8b48b7866f0f6cc7c42abec612f1cc8bd26a779f
sergey.rodionov <srodionov.mail@gmail.com>
2018-05-17 14:08:18 +0500

    vk api

---
+++ friends_add.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+import time
+import vk
+
+text = 'Привет! Лига Индиго открывает регистрацию на первую игру весенней серии! Её проведет спортсменка, комсомолка и просто красавица Полина Асташева! Добавляй нас в друзья, вступай в группу Лига Индиго. Тюмень и следи за новостями!'
+text = '''
+Привет) Совсем скоро Лига Индиго соберёт под одной крышей много умных и талантливых людей.
+Приходи 24 апреля в 19:04 в гриль-бар Колбас-Барабас и блесни логикой, эрудицией, интуицией и чувстовом юмора!
+Вступай в группу Лига Индиго. Тюмень и следи за новостями;)
+'''
+
+text = '''
+Привет) Лига Индиго собирает под одной крышей много умных и талантливых людей.
+Мы только что провели первую игру, и это было круто! Совсем скоро будет готов фотоотчет в нашей группе https://vk.com/li_tyumen.
+Вторая игра не за горами, не пропусти;)
+'''
+
+text = '''
+Привет&#128075;
+[li_tyumen|Лига Индиго] собирает под одной крышей умных, талантливых и веселых людей.
+[li_tyumen|Лига Индиго] - это не просто очередной квиз&#9757; Раунды ligaindigo.ru/raunds настолько разнообразны, что каждый игрок в команде сможет проявить себя.
+Мы уже провели первую игру, и это было круто&#128293; Фотоотчет ты можешь посмотреть в нашей группе [li_tyumen|Лига Индиго. Тюмень].
+Приходи на следующую игру 10 Мая в 19:05 в гриль-бар Колбас-Барабас и блесни логикой, эрудицией, интуицией и чувством юмора &#127891;&#127942;
+[li_tyumen|Cледи за новостями]&#128521;
+'''
+
+text = '''
+Привет&#128075; Лига Индиго собирает под одной крышей умных, талантливых и веселых людей.
+Лига Индиго - это не просто очередной квиз&#9757; Раунды ligaindigo.ru/raunds настолько разнообразны, что каждый игрок в команде сможет проявить себя.
+Мы уже провели первую игру, и это было круто&#128293; Фотоотчет ты можешь посмотреть в нашей группе vk.com/li_tyumen.
+Приходи на следующую игру 10 Мая в 19:05 в гриль-бар Колбас-Барабас и блесни логикой, эрудицией, интуицией и чувством юмора;)
+'''
+
+text = '''
+Привет&#128075; Лига Индиго собирает под одной крышей умных, талантливых и веселых людей.
+Лига Индиго - это не просто очередной квиз&#9757; Раунды ligaindigo.ru/raunds настолько разнообразны, что каждый игрок в команде сможет проявить себя.
+Мы уже провели несколько игр, и это было круто&#128293; Фотоотчеты ты можешь посмотреть в нашей группе vk.com/li_tyumen.
+Следующая игра не за горами, не пропусти;)
+'''
+
+group_id = 'mozgoboj_tmn'
+#group_id = 'ESLPodcast72'
+#vk.users_to_csv(vk.groups_get(group_id),group_id)
+users = vk.users_from_csv(group_id,27274486)
+
+now = datetime.now()
+file_name = now.strftime("%Y%m%d_%H%M%S")
+for user in users:
+    f = open(file_name+'.log', "a")
+    now = datetime.now()
+    print(now.strftime("%Y/%m/%d %H:%M:%S"))
+    f.write(now.strftime("%Y/%m/%d %H:%M:%S") + ',')
+    print(user['id'], user['last_name'], user['first_name'], user['domain'])
+    f.write(str(user['id']) + ',' + user['last_name'] + ',' + user['first_name'] + ',' + user['domain'] + ',')
+    #text = now.strftime("%d/%m/%y %H:%M")
+    response = vk.friends_add(user['id'],text)
+
+    if 'response' in response:
+        print('response:', response['response'])
+        f.write(str(response['response']) + ',')
+    if 'error' in response:
+        print('error_code:', response['error']['error_code'])
+        print('error_msg:', response['error']['error_msg'])
+        f.write('error_code: ' + str(response['error']['error_code']) + ',')
+        f.write('error_msg: ' + response['error']['error_msg'] + ',')
+        if 'captcha_sid' in response['error']:
+            print('captcha_sid:', response['error']['captcha_sid'])
+            print('captcha_img:', response['error']['captcha_img'])
+            f.write('captcha_sid: ' + str(response['error']['captcha_sid']) + ',')
+            f.write('captcha_img: ' + response['error']['captcha_img'] + ',')
+        break
+    f.write('\n')
+    f.close()
+    time.sleep(1200)
---
+++ vk.py
@@ -0,0 +1,152 @@
+import json 
+import requests
+import time
+from datetime import datetime
+import csv
+
+#https://oauth.vk.com/authorize?client_id=6428597&scope=friends,offline&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.73&response_type=token
+api = 'https://api.vk.com/method/'
+access_token = '67430c02481fd18e0a63795f7ff785779855aedeb74d608adf401268a20753dab07d75a7625fc7a12d940'
+v = '5.73'
+
+#Возвращает список идентификаторов друзей пользователя или расширенную информацию о друзьях пользователя
+def friends_get(user_id):
+    method_name = 'friends.get'
+    parameters = {}
+    parameters['access_token'] = access_token
+    parameters['v'] = v
+    parameters['user_id'] = user_id
+    parameters['fields'] = 'sex,domain,nickname,city,last_seen'
+    r = requests.get(api + method_name, parameters)
+    result = json.loads(r.text)['response']['items']
+    print('Friends of', user_id, ':', len(result))
+    return result
+#friends_get('5879128')
+
+#Возвращает список участников сообщества
+def groups_get_members(group_id):
+    method_name = 'groups.getMembers'
+    parameters = {}
+    parameters['access_token'] = access_token
+    parameters['v'] = v
+    parameters['group_id'] = group_id
+    parameters['offset'] = 0
+    result = []
+    while 1==1:
+        r = requests.get(api + method_name, parameters)
+        res = json.loads(r.text)['response']['items']
+        result = result + res
+        parameters['offset'] = parameters['offset'] + 1000
+        if len(res) != 1000:
+            break
+    print('Members of', group_id, ':', len(result))
+    return result
+#groups_get_members('mozgoboj_tmn')
+
+#Возвращает расширенную информацию о пользователях
+def users_get(user_ids):
+    method_name = 'users.get'
+    parameters = {}
+    parameters['access_token'] = access_token
+    parameters['v'] = v
+    parameters['fields'] = 'last_seen,city,domain'
+    parameters['user_ids'] = user_ids
+    r = requests.get(api + method_name, parameters)
+    result = json.loads(r.text)['response'][0]
+    print('User ID', user_ids, 'info:\n', result)
+    return result
+#users_get('mozgobojtmn')
+
+#Возвращает список сообществ указанного пользователя
+def groups_get(group_id):
+    users = []
+    members = groups_get_members(group_id)
+    for member in members:
+        user = users_get(member)
+        users.append(user)
+        time.sleep(0.4)
+    print('Got info about', len(users), 'users of', group_id)
+    return users
+#print(groups_get('ESLPodcast72'))
+
+#Одобряет или создает заявку на добавление в друзья
+def friends_add(user_id, text):
+    method_name = 'friends.add'
+    parameters = {}
+    parameters['access_token'] = access_token
+    parameters['v'] = v
+    parameters['user_id'] = user_id
+    parameters['text'] = text
+    parameters['follow'] = 0
+    parameters['captcha_sid'] = 801054743215
+    parameters['captcha_key'] = 'hed2k'
+    r = requests.get(api + method_name, parameters)
+    return json.loads(r.text)
+
+#Возвращает информацию о полученных или отправленных заявках на добавление в друзья для текущего пользователя
+def friends_get_requests(user_id):
+    method_name = 'friends.getRequests'
+    parameters = {}
+    parameters['access_token'] = access_token
+    parameters['v'] = v
+    parameters['user_id'] = user_id
+    parameters['offset'] = 100
+    parameters['count'] = 1000
+    parameters['out'] = 1
+    r = requests.get(api + method_name, parameters)
+    result = json.loads(r.text)['response']['items']
+    print('User has', len(result), 'requests')
+    return result
+
+#Удаляет пользователя из списка друзей или отклоняет заявку в друзья
+def friends_delete(user_id):
+    method_name = 'friends.delete'
+    parameters = {}
+    parameters['access_token'] = access_token
+    parameters['v'] = v
+    parameters['user_id'] = user_id
+    r = requests.get(api + method_name, parameters)
+    result = json.loads(r.text)
+    print('User', user_id, result)
+    return result
+
+def users_to_csv(users, file_name):
+    csv_rows = []
+    for user in users:
+        if 'deactivated' in user:
+            continue
+        if 'last_seen' in user:
+            last_seen = datetime.fromtimestamp(user['last_seen']['time'])
+            if last_seen.year < datetime.today().year:
+                continue
+        if 'city' in user:
+            if user['city']['title'] != 'Тюмень':
+                continue
+        csv_rows.append({'id':user['id'],'last_name':user['last_name'],'first_name':user['first_name'],'domain':user['domain']})
+        
+    f = open(file_name+'.csv', "w", newline="", encoding='utf-8')
+    writer = csv.DictWriter(f,['id','last_name','first_name','domain'])
+    writer.writerows(csv_rows)
+    f.close()
+    print('Saved', len(csv_rows), 'users to file', file_name)
+
+
+def users_from_csv(file_name, last_id):
+    f = open(file_name+'.csv', "r", newline="", encoding='utf-8')
+    reader = csv.reader(f)
+    users = []
+    for row in reader:
+        if int(row[0]) > last_id:
+            users.append({'id':row[0],'last_name':row[1],'first_name':row[2],'domain':row[3]})
+    #users.sort()
+    f.close()
+    print('Loaded', len(users), 'users from file', file_name)
+    return users
+
+def items_to_csv(items, file_name):
+    f = open(file_name+'.csv', "w", newline="", encoding='utf-8')
+    for item in items:
+        f.write(str(item))
+        f.write('\n')
+    f.close()
+    print('Saved',len(items),'items to',file_name)
