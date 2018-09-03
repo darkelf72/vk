f415b76ba4443cd79089abe29241219f6b913384
rodionov_sa <srodionov.mail@gmail.com>
2018-09-03 17:19:12 +0500

    12

--- friends_add.py
+++ friends_add.py
@@ -1,38 +1,27 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+from datetime import timedelta
 import time
 import vk
 
-next_date = '30.08.2018'
-next_time = '19' + 
-next_place = 'в гриль-бар Колбас-Барабас'
-next_place = 'в ресторан Максимиланс'
+next_date = datetime(2018,9,6)
+next_hour = 19
+next_minute = next_date.month
+next_datetime = next_date + timedelta(hours=next_hour) + timedelta(minutes=next_minute)
+next_place = 'гриль-бар Колбас-Барабас'
+#next_place = 'ресторан Максимиланс'
+next_text = next_datetime.strftime("%d %B %H:%M") + ' в ' + next_place
 
 text = '''
 Привет&#128075; Лига Индиго собирает под одной крышей умных, талантливых и веселых людей.
 Лига Индиго - это не просто очередной квиз&#9757; Раунды ligaindigo.ru/raunds настолько разнообразны, что каждый игрок в команде сможет проявить себя.
 Мы уже провели несколько игр, и это было круто&#128293; Фотоотчеты ты можешь посмотреть в нашей группе vk.com/li_tyumen.
 '''
-
-if datetime.now() > next_date:
+#на pythonanywhere время по гринвичу, поэтому -5 часов
+if datetime.now() > next_datetime - timedelta(hours=5):
     text = text + 'Следующая игра не за горами, не пропусти;)'
 else:
-    text = text + 'Приходи на следующую игру' + next_place + 'и блесни логикой, эрудицией, интуицией и чувством юмора;)'
-
-
-text = '''
-Привет&#128075; Лига Индиго собирает под одной крышей умных, талантливых и веселых людей.
-Лига Индиго - это не просто очередной квиз&#9757; Раунды ligaindigo.ru/raunds настолько разнообразны, что каждый игрок в команде сможет проявить себя.
-Мы уже провели несколько игр, и это было круто&#128293; Фотоотчеты есть в нашей группе vk.com/li_tyumen.
-Приходи на следующую игру 2 Августа в 19:08 в гриль-бар Колбас-Барабас и блесни логикой, эрудицией, интуицией и чувством юмора;)
-'''
-
-text = '''
-Привет&#128075; Лига Индиго собирает под одной крышей умных, талантливых и веселых людей.
-Лига Индиго - это не просто очередной квиз&#9757; Раунды ligaindigo.ru/raunds настолько разнообразны, что каждый игрок в команде сможет проявить себя.
-Мы уже провели несколько игр, и это было круто&#128293; Фотоотчеты есть в нашей группе vk.com/li_tyumen.
-Приходи на следующую игру 16 Августа в 19:08 в ресторан Максимиланс и блесни логикой, эрудицией, интуицией и чувством юмора;)
-'''
+    text = text + 'Приходи на следующую игру ' + next_text + ' и блесни логикой, эрудицией, интуицией и чувством юмора;)'
 
 group_id = 'ESLPodcast72'
 group_id = 'mozgoboj_tmn'
@@ -40,7 +29,7 @@ group_id = 'quizplease_tmn'
 group_id = 'quizium_tmn'
 group_id = 'komnatatyumen'
 
-users = vk.users_from_csv(group_id,6786949)
+users = vk.users_from_csv(group_id,13500481)
 
 mode = 'w'
 file_name = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -59,7 +48,7 @@ for user in users:
     print(csv_row)
     csv_rows.append(csv_row)
     vk.to_csv(csv_rows, file_name, mode)
-    mode = 'a'    
+    mode = 'a'
 
     if 'error' in r:
         if 'captcha_sid' in r['error']:
